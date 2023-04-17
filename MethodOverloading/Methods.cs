1aa8c95ff2592ee5d3a0b92c39fa3dd86e07d6fc
Doug McPeek <dmcpeek@outlook.com>
2023-04-17 10:27:31 -0600

    Initial check in

---
+++ MethodOverloading/Methods.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverloading
+{
+    internal class Methods
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static decimal Add(decimal c, decimal d)
+        {
+            return c + d;
+        }
+
+        public static string Add(int a, int b, bool e)
+        {
+            string numA = a.ToString();
+            string numB = b.ToString();
+
+            if (e == false)
+            {
+                return "1 dollar";
+            }
+            else
+            { 
+                if (a + b == 1) 
+                { 
+                    return "1 dollar";
+                }
+                else
+                {
+                    var sum = a + b;
+                    return (sum + " dollars");
+                }
+            }
+        }
+    }
+}
--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+                Console.WriteLine(Methods.Add(2358, 1234));
+
+                Console.WriteLine(Methods.Add(3.1415926m, 2.356789m));
+
+                Console.WriteLine(Methods.Add(1, 1, true));
         }
     }
 }
